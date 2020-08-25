f3895d8a4751e1be7ef628c714cf2ec44bbbd978
Bhaskar Venkatraman <bhaskar@bhaskars-mbp.home>
2020-08-24 23:33:28 -0400

    add opening article feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React,{useState,useEffect} from 'react';
+import wordsToNumbers from 'words-to-numbers';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from './Components/NewsCards/NewsCards';
 import useStyles from './styles.js';
@@ -11,12 +12,22 @@ const App = () => {
     useEffect(()=> {
         alanBtn({
             key: alanKey,
-            onCommand: ({command,articles}) => {
+            onCommand: ({command,articles,number}) => {
                 if(command === 'newHeadlines'){
                     setNewsArticles(articles);
                     setActiveArticle(-1);
                 } else if(command=== 'highlight'){
                     setActiveArticle((prevActiveArticle)=> prevActiveArticle +1);
+                }
+                else if(command=== 'open') {
+                    const parsedNumber = number.length >2 ? wordsToNumbers(number,{fuzzy:true}) : number;
+                    const article = articles[parsedNumber -1];
+                    if(parsedNumber >20){
+                        alanBtn().playText('Please try again');
+                    }
+                    else if (article){
+                    window.open(article.url,'_blank');}
+                    alanBtn.playText('Opening');
                 }
             }
         })
