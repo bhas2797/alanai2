8edb4e895205ba2c4e925cec42e9e110e4c642c3
Bhaskar Venkatraman <bhaskar@bhaskars-mbp.home>
2020-08-24 23:18:44 -0400

    add scroll

--- src/Components/NewsCard/NewsCard.js
+++ src/Components/NewsCard/NewsCard.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, {useState,useEffect,createRef}from 'react'
 import {Card, CardActions,CardActionArea,CardContent,CardMedia,Button,Typography} from '@material-ui/core';
 import classNames from 'classnames';
 import useStyles from './styles.js';
+
 export default function Newscard({article:{description,publishedAt,source,title,url,urlToImage },i,activeArticle}) {
     const classes = useStyles();
+    const [elRefs,setElRefs] =useState([]);
+    const scrollToRef = (ref) => window.scroll(0,ref.current.offsetTop-50);
+    useEffect(()=> {
+        setElRefs((refs)=> Array(20).fill().map((_,j)=>refs[j]||createRef()));
+    },[]);
+    useEffect(()=> {
+        if(i=== activeArticle &&elRefs[activeArticle]){
+            scrollToRef(elRefs[activeArticle]);
+        }
+    },[i,activeArticle,elRefs])
     return (
-        <Card className={classNames(classes.card,activeArticle===i ? classes.activeCard:null)}>
+        <Card ref={elRefs[i]} className={classNames(classes.card,activeArticle===i ? classes.activeCard:null)}>
             <CardActionArea href={url} target ='_blank'>
                 <CardMedia className={classes.media} image ={urlToImage || 'https://image.shutterstock.com/image-vector/breaking-news-background-world-global-260nw-719766118.jpg'} />
                 <div className = {classes.details}>
